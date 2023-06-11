55d435865d9d6d6e7a9f6f5e92c168a286c14423
2002jan <2002jan@gmail.com>
2023-06-11 12:14:11 +0200

    Created test framework

---
+++ src/test/java/pl/put/poznan/building/classes/BuildingTest.java
@@ -0,0 +1,36 @@
+package pl.put.poznan.building.classes;
+
+import org.junit.jupiter.api.Test;
+
+public class BuildingTest {
+
+    @Test
+    void addRoom() {
+        //TODO: Implement
+    }
+
+    @Test
+    void getArea() {
+        //TODO: Implement
+    }
+
+    @Test
+    void getVolume() {
+        //TODO: Implement
+    }
+
+    @Test
+    void getHeatingEnergy() {
+        //TODO: Implement
+    }
+
+    @Test
+    void getLightingPower() {
+        //TODO: Implement
+    }
+
+    @Test
+    void getRoomsExceedingHeatingEnergy() {
+        //TODO: Implement
+    }
+}
